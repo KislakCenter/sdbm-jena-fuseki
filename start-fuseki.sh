20121bf7ac0b4be5cb474a2d07734893ea97698e
Doug Emery <emeryr@upenn.edu>
2022-09-19 11:16:48 -0400

    Remove reliance on fuseki_confg.ttl

--- start-fuseki.sh
+++ start-fuseki.sh
@@ -2,19 +2,13 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+echo $SCRIPT_DIR
 config_file=$SCRIPT_DIR/.sdbmjfrc
 data_loc=$SCRIPT_DIR/TDB2
-
-if [[ -f $config_file ]]
-then
-  source $config_file
-else
-    echo "WARNING: Config file not present: '$config_file'"
-fi
 
 if [[ ! -d $data_loc ]]
 then
     echo "ERROR: Data directory not present: '$data_loc'"
 fi
 
-fuseki-server --config fuseki_config.ttl
+fuseki-server --loc=TDB2 /sdbm
